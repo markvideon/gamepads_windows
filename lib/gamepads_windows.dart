f80b9a6ccbc378c913d4dd30ce2c97c1e58c331b
Mark Videon <mark.videon@gmail.com>
2024-05-28 18:33:21 +0930

    Removed lib, test, added gitignore and metadata

--- lib/gamepads_windows.dart
+++
@@ -1,8 +0,0 @@
-
-import 'gamepads_windows_platform_interface.dart';
-
-class GamepadsWindows {
-  Future<String?> getPlatformVersion() {
-    return GamepadsWindowsPlatform.instance.getPlatformVersion();
-  }
-}
